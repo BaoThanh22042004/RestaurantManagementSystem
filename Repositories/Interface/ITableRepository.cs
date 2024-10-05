2e81281fdccd11eefef5bb6e733b8492ca0b2bb8
BaoThanh22042004 <thanhpbce181506@fpt.edu.vn>
2024-10-05 16:40:35 +0700

    Reformat code and clean up using directives
    Reformatted ITableRepository and TableRepository to use tabs instead of spaces for indentation. Removed unused using directives from both files. Updated TableRepository methods to use the Table entity directly from the Models.Entities namespace without the full namespace path. Retained and reformatted the Dispose method for consistency.

--- Repositories/Interface/ITableRepository.cs
+++ Repositories/Interface/ITableRepository.cs
@@ -1,20 +1,15 @@
 ﻿using Models.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories.Interface
 {
-    public interface ITableRepository
-    {
-        Task<IEnumerable<Table>> GetAllAsync();
-        Task<Table?> GetByIDAsync(int id);
-        Task InsertAsync(Table table);
-        Task DeleteAsync(int id);
-        Task DeleteAsync(Table table);
-        Task UpdateAsync(Table table);
-        Task SaveAsync();
-    }
+	public interface ITableRepository
+	{
+		Task<IEnumerable<Table>> GetAllAsync();
+		Task<Table?> GetByIDAsync(int id);
+		Task InsertAsync(Table table);
+		Task DeleteAsync(int id);
+		Task DeleteAsync(Table table);
+		Task UpdateAsync(Table table);
+		Task SaveAsync();
+	}
 }
--- Repositories/TableRepository.cs
+++ Repositories/TableRepository.cs
@@ -1,75 +1,71 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Models;
 using Models.Entities;
 using Repositories.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories
 {
-    public class TableRepository : ITableRepository, IDisposable
-    {
-        private readonly DBContext _context;
+	public class TableRepository : ITableRepository, IDisposable
+	{
+		private readonly DBContext _context;
 
-        public TableRepository(DBContext context)
-        {
-            _context = context;
-        }
-        public async Task<IEnumerable<Models.Entities.Table>> GetAllAsync()
-        {
-            return await _context.Tables.AsNoTracking().ToListAsync();
-        }
-        public async Task<Models.Entities.Table?> GetByIDAsync(int id)
-        {
-            return await _context.Tables.FindAsync(id);
-        }
+		public TableRepository(DBContext context)
+		{
+			_context = context;
+		}
 
-        public async Task InsertAsync(Models.Entities.Table table)
-        {
-            await _context.Tables.AddAsync(table);
-            await SaveAsync();
-        }
+		public async Task<IEnumerable<Table>> GetAllAsync()
+		{
+			return await _context.Tables.AsNoTracking().ToListAsync();
+		}
 
-        public async Task DeleteAsync(int id)
-        {
-            var table = await _context.Tables.FindAsync(id);
-            if (table == null)
-            {
-                throw new Exception($"Table with id {id} not found");
-            }
-            await DeleteAsync(table);
-        }
+		public async Task<Table?> GetByIDAsync(int id)
+		{
+			return await _context.Tables.FindAsync(id);
+		}
 
-        public async Task DeleteAsync(Models.Entities.Table table)
-        {
-            if (_context.Entry(table).State == EntityState.Detached)
-            {
-                _context.Tables.Attach(table);
-            }
-            _context.Tables.Remove(table);
-            await SaveAsync();
-        }
+		public async Task InsertAsync(Table table)
+		{
+			await _context.Tables.AddAsync(table);
+			await SaveAsync();
+		}
 
-        public async Task UpdateAsync(Models.Entities.Table table)
-        {
-            _context.Tables.Attach(table);
-            _context.Entry(table).State = EntityState.Modified;
-            await SaveAsync();
-        }
+		public async Task DeleteAsync(int id)
+		{
+			var table = await _context.Tables.FindAsync(id);
+			if (table == null)
+			{
+				throw new Exception($"Table with id {id} not found");
+			}
+			await DeleteAsync(table);
+		}
 
-        public async Task SaveAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
+		public async Task DeleteAsync(Table table)
+		{
+			if (_context.Entry(table).State == EntityState.Detached)
+			{
+				_context.Tables.Attach(table);
+			}
+			_context.Tables.Remove(table);
+			await SaveAsync();
+		}
 
-        public void Dispose()
-        {
-            _context.Dispose();
-            GC.SuppressFinalize(this);
-        }
-    }
+		public async Task UpdateAsync(Table table)
+		{
+			_context.Tables.Attach(table);
+			_context.Entry(table).State = EntityState.Modified;
+			await SaveAsync();
+		}
+
+		public async Task SaveAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+			GC.SuppressFinalize(this);
+		}
+	}
 }
