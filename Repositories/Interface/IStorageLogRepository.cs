8208e6cd97f04feb0e768fd612c38784e20b4175
Dat2807 <lydat28072004@gmail.com>
2024-10-05 16:21:47 +0700

    Add repository and interface of reservation, storage and storage log

---
+++ Repositories/Interface/IStorageLogRepository.cs
@@ -0,0 +1,20 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interface
+{
+    public interface IStorageLogRepository
+    {
+        Task<IEnumerable<StorageLog>> GetAllAsync();
+        Task<StorageLog?> GetByIDAsync(long id); 
+        Task InsertAsync(StorageLog storageLog);
+        Task DeleteAsync(long id); 
+        Task DeleteAsync(StorageLog storageLog);
+        Task UpdateAsync(StorageLog storageLog);
+        Task SaveAsync();
+    }
+}
