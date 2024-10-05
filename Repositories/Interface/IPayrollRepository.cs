b99272109a1e1415c3f2267b58e4f8f28608641f
BaoThanh22042004 <thanhpbce181506@fpt.edu.vn>
2024-10-05 14:12:35 +0700

    Add PayrollRepository with CRUD operations
    Introduce PayrollRepository class implementing IPayrollRepository and IDisposable interfaces. This class provides asynchronous CRUD operations for Payroll entities using Entity Framework Core. Methods include GetAllAsync, GetByIDAsync, InsertAsync, DeleteAsync (by ID and entity), UpdateAsync, and SaveAsync. Implement Dispose method to release database context resources. Add necessary using directives.

---
+++ Repositories/Interface/IPayrollRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models.Entities;
+
+namespace Repositories.Interface
+{
+	public interface IPayrollRepository
+	{
+		Task<IEnumerable<Payroll>> GetAllAsync();
+		Task<Payroll?> GetByIDAsync(long id);
+		Task InsertAsync(Payroll payroll);
+		Task DeleteAsync(long id);
+		Task DeleteAsync(Payroll payroll);
+		Task UpdateAsync(Payroll payroll);
+		Task SaveAsync();
+	}
+}
---
+++ Repositories/PayrollRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.Entities;
+using Repositories.Interface;
+
+namespace Repositories
+{
+	public class PayrollRepository : IPayrollRepository, IDisposable
+	{
+		private readonly DBContext _context;
+
+		public PayrollRepository(DBContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<Payroll>> GetAllAsync()
+		{
+			return await _context.Payrolls.AsNoTracking().ToListAsync();
+		}
+
+		public async Task<Payroll?> GetByIDAsync(long id)
+		{
+			return await _context.Payrolls.FindAsync(id);
+		}
+
+		public async Task InsertAsync(Payroll payroll)
+		{
+			await _context.Payrolls.AddAsync(payroll);
+			await SaveAsync();
+		}
+
+		public async Task DeleteAsync(long id)
+		{
+			var payroll = await _context.Payrolls.FindAsync(id);
+			if (payroll == null)
+			{
+				throw new Exception($"Payroll with id {id} not found");
+			}
+			await DeleteAsync(payroll);
+		}
+
+		public async Task DeleteAsync(Payroll payroll)
+		{
+			if (_context.Entry(payroll).State == EntityState.Detached)
+			{
+				_context.Payrolls.Attach(payroll);
+			}
+			_context.Payrolls.Remove(payroll);
+			await SaveAsync();
+		}
+
+		public async Task UpdateAsync(Payroll payroll)
+		{
+			_context.Payrolls.Attach(payroll);
+			_context.Entry(payroll).State = EntityState.Modified;
+			await SaveAsync();
+		}
+
+		public async Task SaveAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+			GC.SuppressFinalize(this);
+		}
+	}
+}
