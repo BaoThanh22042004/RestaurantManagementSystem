9a6c6ecbebf990560d5327282d698a297af668bb
Dat2807 <lydat28072004@gmail.com>
2024-10-10 11:21:11 +0700

    Add HomeLayout, Menu

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using Repositories.Interface;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-	public class HomeController : Controller
+    public class HomeController : Controller
 	{
-		public IActionResult Index()
+        private readonly IDishRepository _dishRepository;
+
+        public HomeController(IDishRepository dishRepository)
+        {
+            _dishRepository = dishRepository;
+        }
+
+        public IActionResult Index()
 		{
 			return View("HomeView");
 		}
-	}
+
+        public async Task<IActionResult> Menu()
+        {
+            var dishes = await _dishRepository.GetAllAsync();
+            var dishList = dishes.Where(m => m.Visible).Select(dish => new DishViewModel(dish));
+            return View("DishView", dishList);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var dish = await _dishRepository.GetByIDAsync(id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            var dishViewModel = new DishViewModel(dish);
+            return View("DetailsDishView", dishViewModel);
+        }
+    }
 }
