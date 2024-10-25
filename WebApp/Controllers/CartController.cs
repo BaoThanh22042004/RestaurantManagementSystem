2f3314eae7ab2496c6bb677664d1135e708ecee2
BaoThanh22042004 <thanhpbce181506@fpt.edu.vn>
2024-10-25 13:06:56 +0700

    Refactor cart logic and improve UI/UX
    Refactored `CartController` to use the new `CartManager` class for handling cart operations, enhancing code reuse and separation of concerns. Introduced `CartManager` in `CartManager.cs` and registered it as a singleton service in `Program.cs`.
    
    Updated `MenuView.cshtml` to improve the UI/UX of the filter form and menu items, including moving inline styles and scripts to `@section Styles` and `@section Scripts`.
    
    Enhanced `_DashboardLayout.cshtml` and `_HomeLayout.cshtml` for better layout and functionality, including ensuring the sidebar has a minimum height of 100vh and displaying the number of items in the cart on the navigation bar.

--- WebApp/Controllers/CartController.cs
+++ WebApp/Controllers/CartController.cs
@@ -1,132 +1,107 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Models.Entities;
-using Newtonsoft.Json;
-using Repositories;
+﻿using Microsoft.AspNetCore.Mvc;
 using Repositories.Interface;
 using WebApp.Models;
-using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+using WebApp.Utilities;
 
 namespace WebApp.Controllers
 {
-    public class CartController : Controller
-    {
-        private readonly IDishRepository _dishRepository;
+	public class CartController : Controller
+	{
+		private readonly IDishRepository _dishRepository;
+		private readonly CartManager _cartManager;
 
-        public CartController(IDishRepository dishRepository)
-        {
-            _dishRepository = dishRepository;
-        }
+		public CartController(IDishRepository dishRepository, CartManager cartManager)
+		{
+			_dishRepository = dishRepository;
+			_cartManager = cartManager;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-			var cart = GetCartFromCookie();
+		public IActionResult Index()
+		{
+			var cart = _cartManager.GetCartFromCookie(Request);
 			return View("CartView", cart);
 		}
 
-        public async Task<IActionResult> AddToCart(int dishId, string returnUrl)
-        {
-            try
-            {
-                var dishes = await _dishRepository.GetAllAsync();
-                var dish = dishes.Where(d => d.DishId.Equals(dishId)).FirstOrDefault();
-                if (dish != null)
-                {
-                    var cart = GetCartFromCookie();
-                    bool isItemExist = cart.ItemList.Where(d => d.Id.Equals(dish.DishId)).Any();
-                    if (isItemExist)
-                    {
-                        cart.ItemList.Where(d => d.Id.Equals(dish.DishId)).First().Quantity++;
-                    }
-                    else
-                    {
-                        CartItemViewModel cartItemViewModel = new CartItemViewModel(dish);
-                        cart.ItemList.Add(cartItemViewModel);
-                    }
-                    SaveCartToCookie(cart);
-                }
-            }
-            catch (Exception)
-            {
-                TempData["Error"] = "An error occurred while adding dish to cart. Please try again later.";
-                return View("DishView");
-            }
+		public async Task<IActionResult> AddToCart(int dishId, string returnUrl)
+		{
+			try
+			{
+				var dishes = await _dishRepository.GetAllAsync();
+				var dish = dishes.Where(d => d.DishId.Equals(dishId)).FirstOrDefault();
+				if (dish != null)
+				{
+					var cart = _cartManager.GetCartFromCookie(Request);
+					bool isItemExist = cart.ItemList.Where(d => d.Id.Equals(dish.DishId)).Any();
+					if (isItemExist)
+					{
+						cart.ItemList.Where(d => d.Id.Equals(dish.DishId)).First().Quantity++;
+					}
+					else
+					{
+						CartItemViewModel cartItemViewModel = new CartItemViewModel(dish);
+						cart.ItemList.Add(cartItemViewModel);
+					}
+					_cartManager.SaveCartToCookie(cart, Response);
+				}
+			}
+			catch (Exception)
+			{
+				TempData["Error"] = "An error occurred while adding dish to cart. Please try again later.";
+				return View("DishView");
+			}
 
-            if (!string.IsNullOrEmpty(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            return RedirectToAction("Index", "Home");
-        }
+			if (!string.IsNullOrEmpty(returnUrl))
+			{
+				return Redirect(returnUrl);
+			}
+			return RedirectToAction("Index", "Home");
+		}
 
-        public async Task<IActionResult> UpdateQuantity(int dishId, string returnUrl, bool isIncrease)
-        {
-            try
-            {
-                var dish = await _dishRepository.GetByIDAsync(dishId);
-                if (dish != null)
-                {
-                    var cart = GetCartFromCookie();
-                    var cartItem = cart.ItemList.FirstOrDefault(d => d.Id == dish.DishId);
-                    if (cartItem != null)
-                    {
-                        if (isIncrease)
-                        {
-                            cartItem.Quantity++;
-                        }
-                        else if (cartItem.Quantity > 1 && !isIncrease)
-                        {
-                            cartItem.Quantity--;
-                        }
-                        else if(cartItem.Quantity == 1 && !isIncrease)
-                        {
-                            cart.ItemList.Remove(cartItem);
-                        }
-                        SaveCartToCookie(cart);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                TempData["Error"] = "An error occurred while updating dish quantity. Please try again later.";
-                return View("CartView");
-            }
-            if (!string.IsNullOrEmpty(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            return RedirectToAction("Index", "Home");
-        }
+		public async Task<IActionResult> UpdateQuantity(int dishId, string returnUrl, bool isIncrease)
+		{
+			try
+			{
+				var dish = await _dishRepository.GetByIDAsync(dishId);
+				if (dish != null)
+				{
+					var cart = _cartManager.GetCartFromCookie(Request);
+					var cartItem = cart.ItemList.FirstOrDefault(d => d.Id == dish.DishId);
+					if (cartItem != null)
+					{
+						if (isIncrease)
+						{
+							cartItem.Quantity++;
+						}
+						else if (cartItem.Quantity > 1 && !isIncrease)
+						{
+							cartItem.Quantity--;
+						}
+						else if (cartItem.Quantity == 1 && !isIncrease)
+						{
+							cart.ItemList.Remove(cartItem);
+						}
+						_cartManager.SaveCartToCookie(cart, Response);
+					}
+				}
+			}
+			catch (Exception)
+			{
+				TempData["Error"] = "An error occurred while updating dish quantity. Please try again later.";
+				return View("CartView");
+			}
+			if (!string.IsNullOrEmpty(returnUrl))
+			{
+				return Redirect(returnUrl);
+			}
+			return RedirectToAction("Index", "Home");
+		}
 
-        public async Task<IActionResult> ClearCart()
-        {
-            var cart = GetCartFromCookie();
-            cart.ItemList.Clear();
-            SaveCartToCookie(cart);
-            return RedirectToAction("Index", "Cart");
-        }
+		public IActionResult ClearCart()
+		{
+			var cart = new CartViewModel();
+			_cartManager.SaveCartToCookie(cart, Response);
+			return RedirectToAction("Index", "Cart");
+		}
 
-        private const string CartCookieKey = "Cart";
-
-        private CartViewModel GetCartFromCookie()
-        {
-            var cartCookie = Request.Cookies[CartCookieKey];
-            if (string.IsNullOrEmpty(cartCookie))
-            {
-                return new CartViewModel();
-            }
-            return JsonConvert.DeserializeObject<CartViewModel>(cartCookie);
-        }
-
-        private void SaveCartToCookie(CartViewModel cart)
-        {
-            var cartJson = JsonConvert.SerializeObject(cart);
-            CookieOptions options = new CookieOptions
-            {
-                Expires = DateTime.Now.AddDays(7),
-                HttpOnly = true
-            };
-            Response.Cookies.Append(CartCookieKey, cartJson, options);
-        }
-    }
+	}
 }
---
+++ WebApp/Utilities/CartManager.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using WebApp.Models;
+
+namespace WebApp.Utilities
+{
+	public class CartManager
+	{
+		private const string CartCookieKey = "Cart";
+
+		public CartViewModel? GetCartFromCookie(HttpRequest request)
+		{
+			var cartCookie = request.Cookies[CartCookieKey];
+			if (string.IsNullOrEmpty(cartCookie))
+			{
+				return new CartViewModel();
+			}
+			return JsonConvert.DeserializeObject<CartViewModel>(cartCookie);
+		}
+
+		public void SaveCartToCookie(CartViewModel cart, HttpResponse response)
+		{
+			var cartJson = JsonConvert.SerializeObject(cart);
+			CookieOptions options = new CookieOptions
+			{
+				Expires = DateTime.Now.AddDays(7),
+				HttpOnly = true
+			};
+			response.Cookies.Append(CartCookieKey, cartJson, options);
+		}
+
+		public string GetCartItemNumber(HttpRequest request)
+		{
+			var cart = GetCartFromCookie(request);
+			if (cart == null)
+			{
+				return "0";
+			}
+			return cart.ItemList.Count > 99 ? "99+" : cart.ItemList.Count.ToString();
+		}
+	}
+}
