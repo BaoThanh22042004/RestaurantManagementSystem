c0720b5d8ff65b1583728d97ca39d719907a7231
Huynh Gia Bao <baohgce180399@fpt.edu.vn>
2024-10-29 22:08:22 +0700

    Refactor AttendanceRepository to use raw SQL queries

--- Repositories/AttendanceRepository.cs
+++ Repositories/AttendanceRepository.cs
@@ -16,25 +16,42 @@ namespace Repositories
 
 		public async Task<IEnumerable<Attendance>> GetAllAsync()
 		{
-			return await _context.Attendances
+			string sqlGetAllAttendances = "SELECT * FROM Attendances";
+			return await _context.Attendances.FromSqlRaw(sqlGetAllAttendances)
 				.Include(a => a.Schedule)
-				.AsNoTracking().ToListAsync();
+				.ToListAsync();
 		}
 
 		public async Task<Attendance?> GetByIDAsync(long id)
 		{
-			return await _context.Attendances.FindAsync(id);
+			string sqlGetAttendanceById = "SELECT * FROM Attendances WHERE AttendId = {0}";
+			return await _context.Attendances.FromSqlRaw(sqlGetAttendanceById,id).FirstOrDefaultAsync();
 		}
 
-		public async Task InsertAsync(Attendance attendance)
-		{
-			await _context.Attendances.AddAsync(attendance);
-			await SaveAsync();
+		public async Task<Attendance> InsertAsync(Attendance attendance)
+		{			
+
+			string sqlInsertAttendance = @"INSERT INTO Attendances (ScheId,CheckIn,CheckOut,WorkingHours,Status)
+											VALUES ({0}, {1}, {2}, {3}, {4});
+											SELECT * FROM Attendances WHERE AttendId = SCOPE_IDENTITY();";
+
+			var insertedAttendance = await _context.Attendances.FromSqlRaw(sqlInsertAttendance, attendance.ScheId,attendance.CheckIn,attendance.CheckOut,attendance.WorkingHours,attendance.Status).ToListAsync();
+
+			var attendanceInserted = insertedAttendance.FirstOrDefault();
+
+			if (attendanceInserted == null)
+			{
+				throw new Exception("Failed to insert dish category");
+			}
+
+			return attendanceInserted;
 		}
 
 		public async Task DeleteAsync(long id)
 		{
-			var attendance = await _context.Attendances.FindAsync(id);
+			string sqlGetAttendanceById = "SELECT * FROM Attendances WHERE AttendId = {0}";
+
+			var attendance = await _context.Attendances.FromSqlRaw(sqlGetAttendanceById, id).FirstOrDefaultAsync();
 			if (attendance == null)
 			{
 				throw new Exception($"Attendance with id {id} not found");
@@ -44,24 +61,21 @@ namespace Repositories
 
 		public async Task DeleteAsync(Attendance attendance)
 		{
-			if (_context.Entry(attendance).State == EntityState.Detached)
-			{
-				_context.Attendances.Attach(attendance);
-			}
-			_context.Attendances.Remove(attendance);
-			await SaveAsync();
+			string sqlDeleteAttendance = "DELETE FROM Attendances WHERE AttendId = {0}";
+			await _context.Database.ExecuteSqlRawAsync(sqlDeleteAttendance, attendance.AttendId);
 		}
 
 		public async Task UpdateAsync(Attendance attendance)
 		{
-			_context.Attendances.Attach(attendance);
-			_context.Entry(attendance).State = EntityState.Modified;
-			await SaveAsync();
-		}
+			string sqlUpdateAttendance = @"UPDATE Attendances 
+											SET ScheId = {0},
+											CheckIn = {1},
+											CheckOut = {2},
+											WorkingHours = {3},
+											Status = {4}
+											WHERE AttendId = {5}";
 
-		public async Task SaveAsync()
-		{
-			await _context.SaveChangesAsync();
+			await _context.Database.ExecuteSqlRawAsync(sqlUpdateAttendance, attendance.ScheId, attendance.CheckIn, attendance.CheckOut,attendance.WorkingHours, attendance.Status, attendance.AttendId);
 		}
 
 		public void Dispose()
